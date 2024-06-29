0bf6425806aece98b6d0f82af6d00590ecfbce8d
Easter1995 <1073208733@qq.com>
2024-06-29 11:07:27 +0800

    创建socket

--- include/config.h
+++ include/config.h
@@ -13,12 +13,17 @@ typedef struct DNS_CONFIG {
 } DNS_CONFIG;
 /* 程序运行时 */
 typedef struct DNS_RUNTIME {
-    DNS_CONFIG config;        // 服务器配置
-    boolean quit;             // 程序是否退出
+    DNS_CONFIG config;               // 服务器配置
+    boolean quit;                    // 程序是否退出
+    SOCKET server;                   // 接受请求的socket
+    SOCKET client;                   // 与上级连接的socket
+    struct sockaddr_in listen_addr;   // 监听地址
+    struct sockaddr_in upstream_addr; // 上级DNS服务器地址
 } DNS_RUNTIME;
 
 /* 初始化配置 */
 DNS_CONFIG config_init(int argc, char *argv[]);
 /* 初始化运行时 */
 DNS_RUNTIME runtime_init(DNS_CONFIG *config);
+
 #endif
--- src/handler.c
+++ src/handler.c
@@ -5,4 +5,26 @@
 
 void socket_init(DNS_RUNTIME *runtime) {
     WSADATA wsa_data;
+    // 使用winsock2.2版本
+    WSAStartup(MAKEWORD(2, 2), &wsa_data);
+    uint16_t default_port = 53;
+    // 监听请求的socket
+    runtime->server = socket(AF_INET, SOCK_DGRAM, 0);
+    runtime->listen_addr.sin_family = AF_INET; // 使用IPv4
+    runtime->listen_addr.sin_addr.s_addr = INADDR_ANY; // 监听所有本地网络接口的传入数据
+    runtime->listen_addr.sin_port = htons(default_port); // 默认使用53号端口
+    int ret = bind(runtime->server, (struct sockaddr*)&runtime->listen_addr, sizeof(runtime->listen_addr));
+    if (ret < 0) {
+        perror("ERROR: bind faild: %d\n", errno);
+        exit(-1);
+    }
+    // 发出请求的socket
+    runtime->client = socket(AF_INET, SOCK_DGRAM, 0);
+    runtime->listen_addr.sin_family = AF_INET; // 使用IPv4
+    runtime->listen_addr.sin_port = htons(default_port); // 默认使用53号端口
+    // 将点分十进制形式的 IP 地址转换为网络字节序的二进制形式
+    if (inet_pton(AF_INET, runtime->config.upstream_server_IP, &runtime->upstream_addr.sin_addr) <= 0) {
+        perror("ERROR: inet_pton failed\n");
+        exit(-1);
+    }
 }
