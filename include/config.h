380a0f453f28dbba655e3618d24441c1f0f34612
Zdestory <1462793837@qq.com>
2024-06-29 13:04:28 +0800

    创建IDmap

--- include/config.h
+++ include/config.h
@@ -13,8 +13,15 @@ typedef struct DNS_CONFIG {
 } DNS_CONFIG;
 /* 程序运行时 */
 typedef struct DNS_RUNTIME {
-    DNS_CONFIG config;        // 服务器配置
-    boolean quit;             // 程序是否退出
+    DNS_CONFIG config;               // 服务器配置
+    SOCKET server;                   //接受请求的socket
+    SOCKET client;                   //与上级连接的socket
+    IdMap *idmap;                    //ID转换表（存储DNS查询的标识符（ID）与对应查询状态的映射表）
+    uint16_t maxId;                  //上一次请求上级时所使用的ID号
+    LRUCache *lruCache;              //缓存来自上级的查询结果，如果请求在缓存内，则直接回复
+    struct sockaddr_in listenAddr;   //监听地址（请求方地址IP+端口
+    struct sockaddr_in upstreamAddr; //上级DNS服务器地址（IP+端口）
+    boolean quit;                    // 程序是否退出
 } DNS_RUNTIME;
 
 /* 初始化配置 */
--- include/resource.h
+++ include/resource.h
@@ -11,4 +11,24 @@ typedef struct BLOCK_TABLE {
 /* 初始化拦截表 */
 BLOCK_TABLE* block_table_init(char* block_list);
 
+typedef struct idMap {
+    time_t time;             //过期时间
+    uint16_t originalId;     //请求方ID
+    struct sockaddr_in addr; //请求方IP+端口
+} IdMap;
+/*初始化ID转换表*/
+IdMap *initIdMap();
+IdMap getIdMap(IdMap *idMap, uint16_t i);
+int setIdMap(IdMap *idMap, IdMap item, uint16_t curMaxId);
+
+/*LRUCache*/
+typedef struct {
+    int size;           //当前缓存大小
+    int capacity;       //缓存容量
+    struct hash *table; //哈希表
+    /*双向链表，保存所有资源信息，溢出时采用LRU方法从尾部删除*/
+    struct node *head; //头结点，后继指向最近更新的数据
+    struct node *tail; //尾结点，前驱指向最久未更新的数据，溢出时优先删除此结点（前提是他不是host中的）
+} LRUCache;
+
 #endif
